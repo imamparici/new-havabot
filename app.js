305a55fa3d1b8c0bea3beadb075081cd5c2dccb6
imamparici <imm.faris@gmail.com>
2025-07-16 22:32:57 +0700

    first

---
+++ app.js
@@ -0,0 +1,31 @@
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+// Create a new client instance
+
+const client = new Client({
+  authStrategy: new LocalAuth({
+    dataPath: "sessionfolder",
+  }),
+});
+
+// When the client is ready, run this code (only once)
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+// When the client received QR-Code
+client.on("qr", (qr) => {
+  console.log("QR RECEIVED", qr);
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("message_create", (message) => {
+  if (message.body === "!ping") {
+    // send back "pong" to the chat the message was sent in
+    client.sendMessage(message.from, "pong");
+  }
+});
+
+// Start your client
+client.initialize();
